8e9d16c65c944f07a40ec17fa3019d3c3aa84c91
Michel Gokan <michel.gokan@kau.se>
2018-11-11 16:55:44 +0100

    Adding nfv-inspector-vms-openstack-plugin

--- NFV-VMS/server/boot/migration.js
+++ NFV-VMS/server/boot/migration.js
@@ -6,6 +6,7 @@ module.exports = function (app) {
   for(var component in components){
     if(component.match(/^nfv-inspector-vms-/)){
       try {
+        console.log("Running migration in "+component+" under lib/boot/migration...");
         require(__dirname + "/../../node_modules/" + component + "/lib/boot/migration").migrate(app);
       } catch (ex){
         console.log("No migration found in "+component+" under lib/boot/migration");
